7fb419ce979f2a392c26b5061b3964793c376863
sbasile-ch <sbasile@companieshouse.gov.uk>
2018-08-15 12:18:00 +0100

    adds automatic browser opening

--- api-go-lucky.go
+++ api-go-lucky.go
@@ -2,37 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/pkg/browser"
 	"github.com/sbasile-ch/api-go-lucky/config"
 	pkgHndl "github.com/sbasile-ch/api-go-lucky/handlers"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"runtime"
 )
-
-const (
-	ServerIpAddr = "127.0.0.1"
-	ServerPort   = 8080
-)
-
-// open opens the specified URL in the default browser of the user.
-func open(url string) error {
-	var cmd string
-	var args []string
-
-	switch runtime.GOOS {
-	case "windows":
-		cmd = "cmd"
-		args = []string{"/c", "start"}
-	case "darwin":
-		cmd = "open"
-	default: // "linux", "freebsd", "openbsd", "netbsd"
-		cmd = "xdg-open"
-	}
-	args = append(args, url)
-	return exec.Command(cmd, args...).Start()
-}
 
 //__________________________________________________
 func main() {
@@ -49,7 +25,11 @@ func main() {
 	// serve CSS and JS files locally stored in ./static
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	serverAddr := fmt.Sprintf("%s:%d", cfg.ServerIpAddr, cfg.ServerPort)
-	http.ListenAndServe(serverAddr, nil)
-	open(serverAddr)
+	serverAddress := fmt.Sprintf("%s:%d", cfg.ServerIpAddr, cfg.ServerPort)
+
+	//prepare to launch the browser automatically
+	url := fmt.Sprintf("http://%s/index", serverAddress)
+	browser.OpenURL(url)
+
+	http.ListenAndServe(serverAddress, nil)
 }
